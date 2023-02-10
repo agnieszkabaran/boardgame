0c4b7814fa31ee2646fe84f508226421abc219bb
agnieszkabaran <agnieszka.anna.baran@gmail.com>
2023-02-10 12:58:29 +0100

    re44

--- Boardgame/Assets/GameManager.cs
+++ Boardgame/Assets/GameManager.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
+
 {
+  private Rigidbody2D myRigidbody2D;  
+   
     // Start is called before the first frame update
     void Start()
+
     {
-        
+        myRigidbody2D = GetComponet<myRigidbody2D>;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnTriggerStay2D(Collider2D);
+
+
     }
 }
